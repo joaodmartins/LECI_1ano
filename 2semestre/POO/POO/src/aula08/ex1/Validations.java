1f788067db6026620a63c8517474e7ffc37a0384
João Martins <joaodiogomartins@ua.pt>
2024-05-04 09:44:25 +0100

    Some Changes

--- 2semestre/POO/POO/src/aula08/ex1/Validations.java
+++
@@ -1,9 +0,0 @@
-package aula08.ex1;
-
-import java.util.regex.Pattern;
-
-public class Validations {
-    public static boolean validateVehiclePlate(String plate) {
-        return Pattern.matches("^[A-Z]{2}-\\d{2}-\\d{2}|\\d{2}-[A-Z]{2}-\\d{2}|\\d{2}-\\d{2}-[A-Z]{2}|[A-Z]{2}-\\d{2}-[A-Z]{2}|[A-Z]{2}-[A-Z]{2}-\\d{2}|\\d{2}-[A-Z]{2}-[A-Z]{2}$", plate);
-    }
-}
